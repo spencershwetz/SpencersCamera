ac71ebe9b394508e2c20c7bf27c5efa79c1f0a67
Spencer <spencershwetz@gmail.com>
2025-04-05 20:31:48 -0700

    renamed folders added apple watch app

---
+++ SC Watch App/SCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCApp.swift
+//  SC Watch App
+//
+//  Created by spencer on 2025-04-05.
+//
+
+import SwiftUI
+
+@main
+struct SC_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
